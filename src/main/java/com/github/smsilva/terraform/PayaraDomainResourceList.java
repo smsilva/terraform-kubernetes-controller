76931f212fc2f267ad82bf5a9c65dee39da70dd9
Silvio Silva <smsilva@gmail.com>
2021-10-11 15:11:58 -0300

    wip

--- src/main/java/com/github/smsilva/terraform/PayaraDomainResourceList.java
+++
@@ -1,6 +0,0 @@
-package com.github.smsilva.terraform;
-
-import io.fabric8.kubernetes.client.CustomResourceList;
-
-public class PayaraDomainResourceList extends CustomResourceList<PayaraDomainResource> {
-}
