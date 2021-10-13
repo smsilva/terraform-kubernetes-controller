74c0a7a781d5ab30dc9239c3f1c4f4749e8d1d7f
Silvio Silva <smsilva@gmail.com>
2021-10-13 08:06:50 -0300

    Kubernetes Fabric8 Client: 5.5.0

---
+++ src/main/java/com/github/smsilva/k8s/dummy/Dummy.java
@@ -0,0 +1,13 @@
+package com.github.smsilva.k8s.dummy;
+
+import io.fabric8.kubernetes.api.model.Namespaced;
+import io.fabric8.kubernetes.client.CustomResource;
+import io.fabric8.kubernetes.model.annotation.Group;
+import io.fabric8.kubernetes.model.annotation.Version;
+
+@Version(Dummy.VERSION)
+@Group(Dummy.GROUP)
+public class Dummy extends CustomResource<DummySpec, Void> implements Namespaced {
+    public static final String GROUP = "demo.fabric8.io";
+    public static final String VERSION = "v1";
+}
---
+++ src/main/java/com/github/smsilva/k8s/dummy/DummyList.java
@@ -0,0 +1,6 @@
+package com.github.smsilva.k8s.dummy;
+
+import io.fabric8.kubernetes.client.CustomResourceList;
+
+public class DummyList extends CustomResourceList<Dummy> {
+}
