fa7e03781adb88ec5b34c5438cd9efdfac91a0b7
Silvio Silva <smsilva@gmail.com>
2021-10-20 18:41:32 -0300

    Limiting JOB BackoffLimit to 2

--- src/main/java/com/github/smsilva/platform/controller/StackInstanceController.java
+++ src/main/java/com/github/smsilva/platform/controller/StackInstanceController.java
@@ -23,52 +23,64 @@ public class StackInstanceController {
     private static final Logger logger = LoggerFactory.getLogger(StackInstanceController.class);
 
     public static void main(String[] args) throws Exception {
-        try (KubernetesClient client = new DefaultKubernetesClient()) {
+        logger.info("Starting...");
 
-            SharedInformerFactory sharedInformerFactory = client.informers();
+        while (true) {
+            try (KubernetesClient client = new DefaultKubernetesClient()) {
 
-            SharedIndexInformer<StackInstance> sharedIndexInformer = sharedInformerFactory
-                    .sharedIndexInformerFor(StackInstance.class, 30 * 1000L);
+                SharedInformerFactory sharedInformerFactory = client.informers();
 
-            sharedIndexInformer.addEventHandler(new ResourceEventHandler<StackInstance>() {
-                @Override
-                public void onAdd(StackInstance instance) {
-                    onAddHandle(instance, client);
-                }
+                SharedIndexInformer<StackInstance> sharedIndexInformer = sharedInformerFactory
+                        .sharedIndexInformerFor(StackInstance.class, 30 * 1000L);
 
-                @Override
-                public void onUpdate(StackInstance oldResource, StackInstance newResource) {
-                    if (theyAreDifferent(oldResource, newResource)) {
-                        onUpdateHandle(oldResource, newResource);
+                sharedIndexInformer.addEventHandler(new ResourceEventHandler<StackInstance>() {
+                    @Override
+                    public void onAdd(StackInstance instance) {
+                        onAddHandle(instance, client);
                     }
-                }
 
-                @Override
-                public void onDelete(StackInstance StackInstance, boolean b) {
-                    onDeleteHandle(StackInstance, client);
-                }
-            });
+                    @Override
+                    public void onUpdate(StackInstance oldResource, StackInstance newResource) {
+                        if (theyAreDifferent(oldResource, newResource)) {
+                            onUpdateHandle(oldResource, newResource, client);
+                        }
+                    }
 
-            sharedInformerFactory.startAllRegisteredInformers();
+                    @Override
+                    public void onDelete(StackInstance StackInstance, boolean b) {
+                        onDeleteHandle(StackInstance, client);
+                    }
+                });
 
-            TimeUnit.MINUTES.sleep(3);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            e.printStackTrace();
+                sharedInformerFactory.startAllRegisteredInformers();
+
+                logger.info("I'll sleep for 10 minute");
+
+                TimeUnit.MINUTES.sleep(10);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 
-    private static void onAddHandle(StackInstance StackInstance, KubernetesClient client) {
-        logger.info("ADD {}", StackInstance.getMetadata().getSelfLink());
-        logger.info("    {}", StackInstance);
+    private static void onAddHandle(StackInstance stackInstance, KubernetesClient client) {
+        logger.info("ADD {}", stackInstance.getMetadata().getSelfLink());
+        logger.info("    {}", stackInstance);
 
-        ConfigMap configMap = createConfigMap(StackInstance, client);
-        logger.info("Upserted ConfigMap at {} data {}", configMap.getMetadata().getSelfLink(), configMap.getData());
-
-        createJob(StackInstance, client, configMap);
+        reconcile(stackInstance, client);
     }
 
-    private static void createJob(StackInstance stackInstance, KubernetesClient client, ConfigMap configMap) {
+    private static void reconcile(StackInstance StackInstance, KubernetesClient client)  {
+        try {
+            ConfigMap configMap = createConfigMap(StackInstance, client);
+
+            createJob(StackInstance, client, configMap);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private static void createJob(StackInstance stackInstance, KubernetesClient client, ConfigMap configMap) throws Exception {
         String stackInstanceName = stackInstance.getName();
         String jobName = stackInstanceName;
         String namespace = stackInstance.getNamespace();
@@ -103,40 +115,64 @@ public class StackInstanceController {
                         .addNewContainer()
                             .withName("apply")
                             .withImage(image)
-                            .withArgs("apply", "-auto-approve")
+                            .withArgs("apply", "-auto-approve", "-no-color", "-input=false")
                             .withEnvFrom(envFromSourceSecret, envFromSourceConfigMap)
                         .endContainer()
                         .withRestartPolicy("Never")
                     .endSpec()
                 .endTemplate()
+                .withBackoffLimit(2)
             .endSpec()
             .build();
 
-        logger.info("Creating job {}", jobName);
+        logger.info("Creating JOB {}", jobName);
 
         client.batch().v1()
-                .jobs().inNamespace(namespace).createOrReplace(job);
-
-        logger.info("Getting POD List from job: {}", job.getMetadata().getName());
-
-        PodList podList = client.pods()
-                .inNamespace(namespace)
-                .withLabel("job-name", job.getMetadata().getName()).list();
-
-        logger.info("Waiting for Succeeded execution: {}", job.getMetadata().getName());
-
-        client.pods()
-                .inNamespace(namespace)
-                .withName(podList.getItems().get(0).getMetadata().getName())
-                .waitUntilCondition(pod -> pod.getStatus().getPhase().equals("Succeeded"), 1, TimeUnit.MINUTES);
-
-        String joblog = client.batch().v1()
                 .jobs()
                 .inNamespace(namespace)
-                .withName(jobName)
-                .getLog();
+                .createOrReplace(job);
 
-        logger.info(joblog);
+        Thread.sleep(10000);
+
+        try {
+            logger.info("Getting POD List from JOB: {}", job.getMetadata().getName());
+
+            PodList podList = client
+                    .pods()
+                    .inNamespace(namespace)
+                    .withLabel("job-name", job.getMetadata().getName())
+                    .list();
+
+            logger.info("POD List from JOB {}: {}", job.getMetadata().getName(), podList.getItems().size());
+
+            if (podList.getItems().size() > 0) {
+                logger.info("Waiting for Succeeded execution: {}", job.getMetadata().getName());
+
+                try {
+                    client.pods()
+                            .inNamespace(namespace)
+                            .withName(podList.getItems().get(0).getMetadata().getName())
+                            .waitUntilCondition(pod -> pod.getStatus().getPhase().equals("Succeeded"), 1, TimeUnit.MINUTES);
+
+                    String joblog = client.batch().v1()
+                            .jobs()
+                            .inNamespace(namespace)
+                            .withName(jobName)
+                            .getLog();
+
+                    logger.info(joblog);
+                } catch (Exception e) {
+                    logger.error(e.getMessage());
+                }
+            }
+        } finally {
+            logger.info("Deleting job: {}", job.getMetadata().getSelfLink());
+
+//            client.batch().v1()
+//                .jobs()
+//                .inNamespace(namespace)
+//                .delete(job);
+        }
     }
 
     private static ConfigMap createConfigMap(StackInstance stackInstance, KubernetesClient client) {
@@ -145,34 +181,39 @@ public class StackInstanceController {
                 .inNamespace(stackInstance.getMetadata().getNamespace())
                 .withName(stackInstance.getMetadata().getName());
 
-        String region = stackInstance.getSpec().getVars().get("region");
-
         ConfigMapBuilder configMapBuilder = new ConfigMapBuilder()
                 .withNewMetadata()
                 .withName(stackInstance.getName())
                 .endMetadata();
 
         for (Map.Entry<String, String> variable : stackInstance.getVariablesAsEntrySet()) {
-            configMapBuilder.addToData(variable.getKey(), variable.getValue());
+            configMapBuilder.addToData("TF_VAR_" + variable.getKey(), variable.getValue());
         }
 
-        return configMapResource.createOrReplace(configMapBuilder.
-                addToData("STACK_INSTANCE_NAME", stackInstance.getName()).
-                addToData("DEBUG", "0").
-                build());
+        configMapBuilder.
+            addToData("STACK_INSTANCE_NAME", stackInstance.getName()).
+            addToData("DEBUG", "0");
+
+        ConfigMap configMap = configMapResource.createOrReplace(configMapBuilder.build());
+
+        logger.info("Upserted ConfigMap at {} data {}", configMap.getMetadata().getSelfLink(), configMap.getData());
+
+        return configMap;
     }
 
     private static boolean theyAreDifferent(StackInstance oldResource, StackInstance newResource) {
         return !oldResource.getMetadata().getResourceVersion().equals(newResource.getMetadata().getResourceVersion());
     }
 
-    private static void onUpdateHandle(StackInstance oldResource, StackInstance newResource) {
+    private static void onUpdateHandle(StackInstance oldResource, StackInstance newResource, KubernetesClient client) {
         logger.info("UPDATE {} - {}/{}",
                 oldResource.getMetadata().getSelfLink(),
                 oldResource.getMetadata().getResourceVersion(),
                 newResource.getMetadata().getResourceVersion());
-        logger.info("    [{}] {}", oldResource.getMetadata().getResourceVersion(), oldResource);
-        logger.info("    [{}] {}", newResource.getMetadata().getResourceVersion(), newResource);
+        logger.info("    [{} {}] {}", oldResource.getMetadata().getUid(), oldResource.getMetadata().getResourceVersion(), oldResource);
+        logger.info("    [{} {}] {}", newResource.getMetadata().getUid(), newResource.getMetadata().getResourceVersion(), newResource);
+
+        reconcile(newResource, client);
     }
 
     private static void onDeleteHandle(StackInstance stackInstance, KubernetesClient client) {
